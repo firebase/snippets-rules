cbd20e945bfd08237be1709afc5ae5783fbea245
Sam Stern <samstern@google.com>
2018-03-27 14:56:55 -0700

    Rules for RBAC, step 2

--- src/test.ts
+++ src/test.ts
@@ -25,7 +25,7 @@ import * as path from 'path';
 const expectFirestore = require('expect-firestore');
 const serviceAccount = require('../service-account.json');
 
-const mockData = {
+const basicData = {
   stories: [
     {
       key: 'story1',
@@ -37,48 +37,124 @@ const mockData = {
   ]
 };
 
+const rbacData = {
+  stories: [
+    {
+      key: 'story1',
+      fields: {
+        name: 'Story 1',
+        roles: {
+          owneruser: 'owner',
+          readeruser: 'reader'
+        }
+      },
+      collections: {}
+    }
+  ]
+};
+
+async function getAuthenticatedDb() {
+  const database = new expectFirestore.Database({
+    credential: serviceAccount
+  });
+
+  await database.authorize();
+
+  return database;
+}
+
 function getRulesFile(name: string): string {
   const filePath = path.join(__dirname, name);
   return fs.readFileSync(filePath).toString();
+}
+
+function clearMockData(database: any) {
+  // TODO: should not have to call both .data and .collections
+  database.data = undefined;
+  database.collections = undefined;
+  database.rules = undefined;
+}
+
+function setMockDataAndRules(database: any, data: any, rulesFile: string) {
+  database.data = data;
+  database.collections = data;
+  database.rules = getRulesFile(rulesFile);
 }
 
 describe('[Basic Rules]', () => {
   let database: any;
 
   before(async () => {
-    database = new expectFirestore.Database({
-      credential: serviceAccount
-    });
-
-    await database.authorize();
+    database = await getAuthenticatedDb();
   });
 
   afterEach(() => {
-    // TODO: should not have to call both .data and .collections
-    database.data = undefined;
-    database.collections = undefined;
-    database.rules = undefined;
+    clearMockData(database);
   });
 
   it('should allow a read at any path to open rules', async () => {
-    const rules = getRulesFile('../rules/open.rules');
-
-    database.data = mockData;
-    database.collections = mockData;
-    database.rules = rules;
+    setMockDataAndRules(database, basicData, '../rules/open.rules');
 
     const allowed = await database.canGet({}, 'any/path');
     assert.ok(allowed, 'Read is allowed');
   });
 
   it('should deny a read any any path to closed rules', async () => {
-    const rules = getRulesFile('../rules/closed.rules');
-
-    database.data = mockData;
-    database.collections = mockData;
-    database.rules = rules;
+    setMockDataAndRules(database, basicData, '../rules/closed.rules');
 
     const allowed = await database.canGet({}, 'any/path');
     assert.ok(!allowed, 'Read is not allowed');
+  });
+});
+
+describe('[RBAC Rules]', () => {
+  let database: any;
+
+  before(async () => {
+    database = await getAuthenticatedDb();
+  });
+
+  afterEach(() => {
+    clearMockData(database);
+  });
+
+  it('[step2] only owners should be allowed to create stories', async () => {
+    setMockDataAndRules(
+      database,
+      rbacData,
+      '../rules/solution-rbac/step2.rules'
+    );
+
+    const auth = {
+      uid: 'user1234'
+    };
+
+    const ownerStory = {
+      name: 'New Story',
+      roles: {
+        user1234: 'owner'
+      }
+    };
+
+    const readerStory = {
+      name: 'New Story',
+      roles: {
+        user1234: 'reader'
+      }
+    };
+
+    const ownerAllowed = await database.canSet(
+      auth,
+      'stories/newstory',
+      ownerStory
+    );
+    assert.ok(ownerAllowed, 'Owner story is allowed.');
+
+    const readerAllowed = await database.canSet(
+      auth,
+      'stories/newstory',
+      readerStory
+    );
+    assert.ok(!readerAllowed, 'Reader story is not allowed.');
   });
 });
